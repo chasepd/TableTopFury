220066944d25d808d3ebc3ce34063a1533442969
Chase Davies <chase.davies@sproutsocial.com>
2023-03-27 10:38:48 -0600

    Started implementing settings mode

--- Modes/SettingsMode.cs
+++ Modes/SettingsMode.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace TableTopFury.Modes
 {
-    internal class SettingsMode
+    internal class SettingsMode : Mode
     {
+        public SettingsMode() : base() { }
+
+        public override Mode CheckForModeChange()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
